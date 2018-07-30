19eb66faf293957e07438f7ea9b2dd0c912904af
Hugo CAPOCCI <hcapocci@degetel.com>
2018-07-30 22:07:44 +0200

    First bdd test written, it fails

---
+++ kata_potter/steps/oneBooks.js
@@ -0,0 +1,11 @@
+const { Given, When, Then } = require('cucumber')
+
+Given('One book', function () {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending';
+});
+
+Then('The book price is {int} euros', function (int) {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending';
+});
