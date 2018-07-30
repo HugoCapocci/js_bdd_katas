4f2ae3d442a1747a357e67d551d645dcab82ac42
Hugo CAPOCCI <hcapocci@degetel.com>
2018-07-30 23:18:26 +0200

    Implements second tests. still failing

---
+++ kata-potter/basket.js
@@ -0,0 +1,13 @@
+module.exports = class Basket {
+  constructor() {
+
+  }
+
+  addBook(book) {
+
+  }
+
+  getPrice() {
+    return 0;
+  }
+}
--- steps/no-discount-for-one-book.js
+++ steps/no-discount-for-one-book.js
@@ -1,16 +1,21 @@
 const { Given, When, Then } = require('cucumber');
 const { expect } = require('chai');
 
+const Basket = require('../kata-potter/basket');
+const Book = require('../kata-potter/book');
+
+let basket;
+
 Given('A basket', function () {
   // Write code here that turns the phrase above into concrete actions
-  return 'pending';
+  basket = new Basket();
 });
 When('I add a book to basket', function () {
   // Write code here that turns the phrase above into concrete actions
-  return 'pending';
+  basket.addBook(new Book());
 });
 
 Then('The basket price is {int} euros', function (int) {
   // Write code here that turns the phrase above into concrete actions
-  return 'pending';
+  expect(basket.getPrice()).to.equal(int);
 });
