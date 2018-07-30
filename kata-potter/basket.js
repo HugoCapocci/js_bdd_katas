06ead739452fb3ec871ed4e4c767ea0b0332836f
Hugo CAPOCCI <hcapocci@degetel.com>
2018-07-30 23:59:53 +0200

    Make 4th test pass

--- kata-potter/basket.js
+++ kata-potter/basket.js
@@ -8,10 +8,12 @@ module.exports = class Basket {
   }
 
   getPrice() {
-    const sum = this.books.reduce((sum, book) => {
-      return sum + book.getPrice();
-    }, 0);
-    if (this.books.length === 2)
+    const volumes = this.books.map(b => b.volume);
+    const uniqueVolumes = volumes.filter((volume, pos) => volumes.indexOf(volume) == pos);
+    const sum = this.books.reduce((sum, book) =>
+      sum + book.getPrice()
+    , 0);
+    if (uniqueVolumes.length === 2)
       return 0.95 * sum;
     return sum;
   }
